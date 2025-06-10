0d55b0e11042c25cf2bdec47bc0dc08517a4a0d5
lafiagi <skillfullmallam@gmail.com>
2025-06-10 19:45:39 +0100

    Signals update

--- pizza/signals.py
+++ pizza/signals.py
@@ -17,13 +17,3 @@ def update_stock_on_order_creation(sender, instance, created, **kwargs):
         if pizza.quantity_in_stock <= 0:
             pizza.is_available = False
             pizza.save(update_fields=["is_available"])
-
-        for extra in instance.extras.all():
-            # Deduct the quantity of the order for each extra
-            extra.quantity_in_stock = F("quantity_in_stock") - instance.quantity
-            extra.save(update_fields=["quantity_in_stock"])
-            extra.refresh_from_db()
-
-            if extra.quantity_in_stock <= 0:
-                extra.is_available = False
-                extra.save(update_fields=["is_available"])
